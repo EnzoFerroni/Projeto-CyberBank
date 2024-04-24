dabca57709992aad357433a2bcf8a0aaf1136ccd
Enzo Ferroni <125482399+EnzoFerroni@users.noreply.github.com>
2024-04-24 09:15:49 -0300

    Spring Initializr

---
+++ CyberBank/src/main/java/bank/cyber/CyberBank/CyberBankApplication.java
@@ -0,0 +1,13 @@
+package bank.cyber.CyberBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberBankApplication.class, args);
+	}
+
+}
