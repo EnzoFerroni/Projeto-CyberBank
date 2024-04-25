1d5a703690c7d22c69cf9d10953ac6ae14d50a42
RNeves <73305741+OliveiraRNeves@users.noreply.github.com>
2024-04-25 14:57:19 -0300

    Add files via upload

---
+++ CyberBank/src/main/java/bank/cyber/CyberBank/TipoContaRepo.java
@@ -0,0 +1,10 @@
+package classesBase;
+//Enzo Ferroni - RA: 10417100
+//Leonardo Rodrigues - RA: 10418105
+//Rafael Neves - RA: 10418316
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TipoContaRepo extends CrudRepository<TipoConta, Long> {
+
+}
